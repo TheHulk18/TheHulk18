5a6e3f8c688966a57233d79f97e0028a8a770c2b
TheHulk18 <152805459+TheHulk18@users.noreply.github.com>
2025-03-19 20:26:26 +0530

    I have completed the assignments.

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+print("Welcome")
+
+
+fname=input("Enter your first name:")
+lname=input("Enter yous last name:")
+
+full_name = fname+ " " +lname
+
+print(f"\nHello, {full_name}! Welcome to the python program")
