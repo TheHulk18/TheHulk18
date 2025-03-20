fad14d7247d940a32a339020d6951715187cd829
TheHulk18 <152805459+TheHulk18@users.noreply.github.com>
2025-03-20 20:09:06 +0530

    Delete Task_2.py

--- Task_2.py
+++
@@ -1,9 +0,0 @@
-print("Welcome")
-
-
-fname=input("Enter your first name:")
-lname=input("Enter yous last name:")
-
-full_name = fname+ " " +lname
-
-print(f"\nHello, {full_name}! Welcome to the python program")
